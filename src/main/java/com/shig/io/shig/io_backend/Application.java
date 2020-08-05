cba1d22609f3b34f116c406ebc51858adc634bdc
Stephen Higgins <stephen.higgins@cofficient.co.uk>
2020-08-05 18:57:02 +0100

    initial commit

---
+++ src/main/java/com/shig/io/shig/io_backend/Application.java
@@ -0,0 +1,13 @@
+package com.shig.io.shig.io_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
